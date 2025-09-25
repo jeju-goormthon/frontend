319f0579d17bbafc06d8c6c222f93575fdfa3434
HarrySeop <harryseop@gmail.com>
2025-09-25 15:19:12 +0900

    [#16] Style: 프레임 수정 아이콘 색상 수정

--- src/layouts/RootFrame.tsx
+++ src/layouts/RootFrame.tsx
@@ -2,8 +2,8 @@ import { Outlet } from 'react-router-dom';
 
 export default function RootFrame() {
   return (
-    <div className='bg-v-gray-200 min-h-dvh'>
-      <main className='border-v-gray-300 mx-auto min-h-dvh max-w-md border-x bg-white'>
+    <div className='min-h-dvh'>
+      <main className='mx-auto min-h-dvh max-w-md bg-white'>
         <Outlet />
       </main>
     </div>
